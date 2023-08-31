a3f9473d6ad836e455575a3e673cbeb9ebbee23f
Valerii Stolbov <stolbov.v@outlook.com>
2023-08-31 19:19:35 +0500

    refactor

--- src/components/list-item/list-item.jsx
+++ src/components/list-item/list-item.jsx
@@ -1,21 +1,42 @@
+import { Component } from 'react';
 import './list-item.css';
 
-function ListItem({ name, salary, vacation, promotion, onUpdateEmployee, onDelEmployee }) {
-  return (
-    <li className={'list-item' + (promotion ? ' list-item_promotion' : '')}>
-      <span className="list-item__name">{name}</span>
-      <span className="list-item__salary">{salary} €</span>
-      <button className={'list-item__button' + (vacation ? ' is-active' : '')} onClick={() => onUpdateEmployee('vacation')}>
-        <i className="bx bxs-plane"></i>
-      </button>
-      <button className={'list-item__button' + (promotion ? ' is-active' : '')} onClick={() => onUpdateEmployee('promotion')}>
-        <i className="bx bx-euro"></i>
-      </button>
-      <button className="list-item__button list-item__button_danger" onClick={onDelEmployee}>
-        <i className="bx bxs-trash"></i>
-      </button>
-    </li>
-  );
+class ListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      vacation: false,
+      promotion: false,
+    };
+  }
+
+  toggleVacation = () => this.setState(({ vacation }) => ({ vacation: !vacation }));
+  togglePromotion = () => this.setState(({ promotion }) => ({ promotion: !promotion }));
+
+  render() {
+    const { name, salary } = this.props;
+    const { vacation, promotion } = this.state;
+
+    const classModifiers = (vacation ? ' list-item_vacation' : '') + (promotion ? ' list-item_promotion' : '');
+
+    return (
+      <li className={'list-item' + classModifiers}>
+        <span className="list-item__name" tabIndex="0" onClick={this.togglePromotion}>
+          {name}
+        </span>
+        <span className="list-item__salary">{salary} €</span>
+        <button className="list-item__button" onClick={this.toggleVacation}>
+          <i className="bx bxs-plane"></i>
+        </button>
+        <button className="list-item__button list-item__button_danger">
+          <i className="bx bxs-trash"></i>
+        </button>
+        <div className="list-item__icon">
+          <i className="bx bx-euro"></i>
+        </div>
+      </li>
+    );
+  }
 }
 
 export { ListItem };
