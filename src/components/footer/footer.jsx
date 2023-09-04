7c886a7d4bb39b794764ed89ffb05220dc215754
Valerii Stolbov <stolbov.v@outlook.com>
2023-09-04 18:24:56 +0500

    add link

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,9 @@ function Footer() {
   return (
     <footer className="footer">
       <span className="footer__year">2023</span>
-      <span className="footer__developer">StolbovVV</span>
+      <span className="footer__developer">
+        Developer: <a href="https://github.com/stolbovvv">stolbovvv</a>
+      </span>
     </footer>
   );
 }
