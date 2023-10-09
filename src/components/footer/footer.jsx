94f70ad5fbd513c7fac902866afb49d17d3a8abf
Valerii Stolbov <stolbov.v@outlook.com>
2023-10-10 01:58:27 +0500

    update footer.jsx

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,10 +3,13 @@ import './footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <span className="footer__year">2023</span>
       <span className="footer__developer">
-        Developer: <a href="https://github.com/stolbovvv">stolbovvv</a>
+        Developer:{' '}
+        <a href="https://github.com/stolbovvv" target="_blank" rel="noopener noreferrer">
+          stolbovvv
+        </a>
       </span>
+      <span className="footer__year">2023</span>
     </footer>
   );
 }
