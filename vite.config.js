4682a241d6ec809672707f47343af5f508f23006
Valerii Stolbov <stolbov.v@outlook.com>
2024-04-11 17:32:24 +0500

    Update URL

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/practice-react-employees/",
+  base: "/practice-employees/",
   plugins: [react()],
 });
